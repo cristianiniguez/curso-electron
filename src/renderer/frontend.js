670d89f168de8bd1a87ef47a3020a6a9e184b2a2
cristianiniguez <felizpc1@gmail.com>
2020-09-03 19:32:53 -0400

    Buscar imágenes

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,5 +1,9 @@
+import url from 'url'
+import path from 'path'
+
 window.addEventListener('load', () => {
   addImageEvent()
+  searchImagesEvent()
 })
 
 function addImageEvent() {
@@ -15,4 +19,29 @@ function changeImage(node) {
   document.querySelector('li.selected').classList.remove('selected')
   node.classList.add('selected')
   document.getElementById('image-displayed').src = node.querySelector('img').src
+}
+
+function searchImagesEvent() {
+  const searchBox = document.getElementById('search-box')
+  searchBox.addEventListener('keyup', function () {
+    const regex = new RegExp(this.value.toLowerCase(), 'gi')
+    if (this.value.length > 0) {
+      const thumbs = document.querySelectorAll('li.list-group-item img')
+      for (let i = 0; i < thumbs.length; i++) {
+        const fileUrl = url.parse(thumbs[i].src)
+        const fileName = path.basename(fileUrl.pathname)
+        if (fileName.match(regex)) {
+          thumbs[i].parentNode.classList.remove('hidden')
+        } else {
+          thumbs[i].parentNode.classList.add('hidden')
+        }
+      }
+      selectFirstImage()
+    }
+  })
+}
+
+function selectFirstImage() {
+  const image = document.querySelector('li.list-group-item:not(.hidden)')
+  changeImage(image)
 }
