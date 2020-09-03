12646e55901f891eb400d30401e8c9ac49843e54
cristianiniguez <felizpc1@gmail.com>
2020-09-03 13:28:08 -0400

    Nuestra primera ventana

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const { app, BrowserWindow } = require('electron')
+
+app.on('before-quit', () => {
+  console.log('Saliendo')
+})
+
+app.on('ready', () => {
+  let win = new BrowserWindow()
+  win.on('closed', () => {
+    win = null
+    app.quit()
+  })
+})
