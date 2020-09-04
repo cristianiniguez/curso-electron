8ca9001c562bd50e6f28e2100806fb0f4c58ebdf
cristianiniguez <felizpc1@gmail.com>
2020-09-04 16:02:01 -0400

    Generar listas de imagenes

--- src/index.js
+++ src/index.js
@@ -54,6 +54,9 @@ ipcMain.on('open-directory', event => {
       const images = []
       if (dir) {
         fs.readdir(dir.filePaths[0], (err, files) => {
+          if (err) {
+            throw err
+          }
           for (let i = 0; i < files.length; i++) {
             if (isImage(files[i])) {
               let imageFile = path.join(dir.filePaths[0], files[i])
@@ -66,7 +69,7 @@ ipcMain.on('open-directory', event => {
               })
             }
           }
-          console.log(images)
+          event.sender.send('load-images', images)
         })
       }
     })
