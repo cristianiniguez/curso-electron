7d3e6f6bc433759207b8a5c2567d0df81be25508
cristianiniguez <felizpc1@gmail.com>
2020-09-05 19:09:26 -0400

    Subir una imagen

--- src/renderer/main-window/ipcRendererEvents.js
+++ src/renderer/main-window/ipcRendererEvents.js
@@ -2,6 +2,8 @@ import { ipcRenderer, remote } from 'electron'
 import settings from 'electron-settings'
 import { addImagesEvent, clearImages, loadImages, selectFirstImage } from './images-ui'
 import { saveImage } from './filters'
+import Cloudup from 'cloudup-client'
+import crypto from 'crypto'
 import path from 'path'
 import os from 'os'
 
@@ -22,6 +24,7 @@ function setIpc() {
       if (err) {
         showDialog('error', 'Platzipics', err.message)
       } else {
+        document.getElementById('image-displayed').dataset.filtered = file
         showDialog('info', 'Platzipics', 'La imagen fue guardada')
       }
     })
@@ -71,9 +74,49 @@ function showDialog(type, title, msg) {
   })
 }
 
+function uploadImage() {
+  let imageNode = document.getElementById('image-displayed')
+  let image
+  if (imageNode.dataset.filtered) {
+    image = imageNode.dataset.filtered
+  } else {
+    image = imageNode.src
+  }
+  image = image.replace('file:///', '')
+  let fileName = path.basename(image)
+  if (settings.hasSync('cloudup.user') && settings.hasSync('cloudup.passwd')) {
+    let textParts = settings.getSync('cloudup.passwd').split(':')
+    let iv = Buffer.from(textParts.shift(), 'hex')
+    let encryptedText = Buffer.from(textParts.join(':'), 'hex')
+    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from('jhdskhfk234423fsdf6sd43444df63dd'), iv)
+    let decrypted = decipher.update(encryptedText)
+    decrypted += decipher.final('utf8')
+
+    const client = Cloudup({
+      user: settings.getSync('cloudup.user'),
+      pass: decrypted
+    })
+
+    const stream = client.stream({
+      title: `Platzipics - ${fileName}`
+    })
+    stream.file(image).save((err) => {
+      if (err) {
+        showDialog('error', 'Platzipics', 'Verifique su conexión o sus credenciales de cloudup')
+        console.error(err)
+      } else {
+        showDialog('info', 'Platzipics', `Imagen cargada con éxito - ${stream.url}`)
+      }
+    })
+  } else {
+    showDialog('error', 'Platzipics', 'Por favor complete las preferencias de cloudup')
+  }
+}
+
 module.exports = {
   setIpc: setIpc,
   openDirectory: openDirectory,
   saveFile: saveFile,
-  openPreferences: openPreferences
+  openPreferences: openPreferences,
+  uploadImage: uploadImage
 }
