3ad4fc1784641f35f75ee495cf428caa475248c1
cristianiniguez <felizpc1@gmail.com>
2020-09-05 17:32:11 -0400

    Creando un ícono en la bandeja del sistema

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Tray } from 'electron'
 import devtools from './devtools'
 import handleErrors from './handle-errors'
 import setMainIpc from './ipcMainEvents'
+import os from 'os'
+import path from 'path'
 
 global.win
+global.tray
 
 if (process.env.NODE_ENV === 'development') {
   devtools()
@@ -39,6 +42,17 @@ app.on('ready', () => {
   global.win.on('closed', () => {
     global.win = null
     app.quit()
+  })
+  let icon
+  if (os.platform() === 'win32') {
+    icon = path.join(__dirname, 'assets', 'icons', 'tray-icon.ico')
+  } else {
+    icon = path.join(__dirname, 'assets', 'icons', 'tray-icon.png')
+  }
+  global.tray = new Tray(icon)
+  global.tray.setToolTip('Platzipics')
+  global.tray.on('click', () => {
+    global.win.isVisible() ? global.win.hide() : global.win.show()
   })
   global.win.loadURL(`file://${__dirname}/renderer/index.html`)
   // win.toggleDevTools()
