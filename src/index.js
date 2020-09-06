8f5393e98c8181c9aee3b775e9279de57696ba7a
cristianiniguez <felizpc1@gmail.com>
2020-09-06 13:07:28 -0400

    Atajos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, BrowserWindow, Tray } from 'electron'
+import { app, BrowserWindow, Tray, globalShortcut } from 'electron'
 import devtools from './devtools'
 import handleErrors from './handle-errors'
 import setMainIpc from './ipcMainEvents'
@@ -15,14 +15,14 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.on('before-quit', () => {
-  console.log('Saliendo')
+  globalShortcut.unregisterAll()
 })
 
 app.on('ready', () => {
   global.win = new BrowserWindow({
     width: 800,
     height: 600,
-    title: 'Hola Mundo',
+    title: 'Platzipics',
     center: true,
     maximizable: false,
     show: false,
@@ -30,14 +30,14 @@ app.on('ready', () => {
       nodeIntegration: true
     }
   })
+  globalShortcut.register('CommandOrControl+Alt+p', () => {
+    global.win.show()
+    global.win.focus()
+  })
   setMainIpc(global.win)
   handleErrors(global.win)
   global.win.once('ready-to-show', () => {
     global.win.show()
-  })
-  global.win.on('move', () => {
-    const position = global.win.getPosition()
-    console.log(position)
   })
   global.win.on('closed', () => {
     global.win = null
